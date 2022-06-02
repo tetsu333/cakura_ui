f1d1999c234f0f6c886e82e551b197cf20983bbb
tetsu333 <70054989+tetsu333@users.noreply.github.com>
2022-06-03 00:53:40 +0900

    ヘッダー作成まで

--- src/router/HomeRoutes.tsx
+++ src/router/HomeRoutes.tsx
@@ -1,6 +1,7 @@
 import { Home } from "../components/pages/Home";
 import { Setting } from "../components/pages/Setting";
 import { UserManagement } from "../components/pages/UserManagement";
+import { Page404 } from "../components/pages/Page404";
 
 export const homeRoutes = [
   {
@@ -17,5 +18,10 @@ export const homeRoutes = [
     path: "/setting",
     exact: false,
     children: <Setting />
+  },
+  {
+    path: "*",
+    exact: false,
+    children: <Page404 />
   }
 ];
